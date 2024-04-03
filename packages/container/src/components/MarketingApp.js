6dae2a9fb6815a42b3df251514a41a1fc78d00d1
Luján <marialscr@ext.inditex.com>
2024-04-03 13:13:51 +0200

    Initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,18 @@
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect} from 'react'
+
+const MarketingApp = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if(ref?.current)  {
+            mount(ref.current);
+        }
+    }, [])
+
+    return <div ref={ref} />;
+}
+
+
+export default MarketingApp;
+
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,24 @@
+/*
+
+Babel procesa los ficheros js y jsx, transforma ES6 y otros años y los transforma en ES5 para el navegador y agrega más características 
+
+
+*/
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-react', '@babel/preset-env'],
+                        plugins: [ '@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
+    }
+};
